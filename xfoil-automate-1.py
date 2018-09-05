56845543c0e28dd01ff459b3a5d493421a1befd4
pranshupant <pranshupant@gmail.com>
2018-09-05 17:11:28 +0530

    scrapped the stdin function method for taking input from .xfoil file

--- xfoil-automate-1.py
+++ xfoil-automate-1.py
@@ -6,35 +6,10 @@ import time
 import os
 import signal
 
-ps = sp.Popen(["xfoil"],
+ps = sp.Popen(['xfoil <controlfile.xfoil>outputfile.out'],
               stdin=sp.PIPE,
               stdout=None,
-              stderr=None,preexec_fn=os.setsid)
-
-def issueCmd(cmd,echo=True):
-    ps.stdin.write(cmd.encode('utf-8'))
-    if echo:
-       print(cmd)
-       #time.sleep(0.5)
-
-cmd = [ "load bsairfoil.txt\n",
-        "oper\n",
-        "v\n",
-        "1.346e6\n", 
-        "iter 5000\n", 
-        "pacc\n",
-        "bsa.txt\n\n",
-        "alfa 0\n",
-        "pacc\n",
-        "cpwr cp_a0.dat\n",
-        "dump d.dat\n",
-        "hard\n",
-        "\n",
-        "quit\n"]
-
-
-for i in range(len(cmd)):
-    issueCmd(cmd[i])
-
-
-#os.killpg(os.getpgid(ps.pid), signal.SIGTERM)
+              stderr=None,
+              shell=True
+              #preexec_fn=os.setsid
+                )
