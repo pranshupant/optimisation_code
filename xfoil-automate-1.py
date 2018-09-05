547d565d782bd0f0ea9f44f2f0aa750b6159b66a
pranshupant <pranshupant@gmail.com>
2018-09-05 18:28:46 +0530

    removed unneccesary imports

--- xfoil-automate-1.py
+++ xfoil-automate-1.py
@@ -2,13 +2,10 @@ import subprocess as sp
 import shutil
 import sys
 import string
-import time
-import os
-import signal
 
-ps = sp.Popen(['xfoil <controlfile.xfoil>outputfile.out'],
-              stdin=sp.PIPE,
-              stdout=None,
-              stderr=None,
-              shell=True
-              )
+sp.Popen(['xfoil <controlfile.xfoil>outputfile.out'],
+      stdin=sp.PIPE,
+      stdout=None,
+      stderr=None,
+      shell=True
+      )
