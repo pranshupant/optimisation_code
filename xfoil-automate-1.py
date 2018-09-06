79563b83ab8994b82c2ae7d710acd01020e62306
pranshupant <pranshupant@gmail.com>
2018-09-06 18:39:17 +0530

    testing new stuff

--- xfoil-automate-1.py
+++ xfoil-automate-1.py
@@ -2,10 +2,14 @@ import subprocess as sp
 import shutil
 import sys
 import string
+import time as t
 
+t1 = t.time()
 sp.Popen(['xfoil <controlfile.xfoil>outputfile.out'],
       stdin=sp.PIPE,
       stdout=None,
       stderr=None,
       shell=True
       )
+t2= t.time()
+print(t2-t1)
