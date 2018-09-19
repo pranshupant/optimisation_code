98ed208e9981d5f9e17adb50fc7dae6f0859fc6d
pranshupant <pranshupant@gmail.com>
2018-09-20 02:30:26 +0530

    made use of copy.deepcopy() to mitigate the effects of copying with reference in python.

--- reproduction.py
+++ reproduction.py
@@ -1,11 +1,12 @@
 from constants import Cmax, Cmin, nPop
-from airfoil_Class import baby_airfoil
+from airfoil_Class import airfoil #baby_airfoil
 import random
+import copy
 
-def reproduction(Airfoil, gen, sigma, i, s):  
+def reproduction(Airfoil, gen, sigma, x, s):  
                      
     costs = []
-
+    
     for t in range(len(Airfoil)):
         costs.append(Airfoil[t].cost)
 
@@ -13,29 +14,38 @@ def reproduction(Airfoil, gen, sigma, i, s):
 
     WorstCost = min(costs)
 
-    ratio = (Airfoil[i].cost - WorstCost)/(BestCost - WorstCost)
+    ratio = (Airfoil[x].cost - WorstCost)/(BestCost - WorstCost)
     C = int(Cmin + (Cmax - Cmin)*ratio)
-    print(Airfoil[i].cost)
+    print(Airfoil[x].cost)
     print(C)
 
     if C > 0:
 
         progeny = []
-
+       
         for j in range(C):
-        
-            progeny.append(baby_airfoil(Airfoil[i], gen, s[0]))
+            
+            p = copy.deepcopy(Airfoil[x])
+            #print(Airfoil[x].lPoint)
+            p.copy_mutate(gen, s[0])
+            progeny.append(p)
 
             progeny[j].new(sigma)
             progeny[j].bspline()
             progeny[j].write()
             progeny[j].savefig()
-
-                     
+            progeny[j].show(gen, s[0])
+                                 
             s[0] += 1
 
         for j in range(C):
 
             progeny[j].xFoil()
+            print(progeny[j].cost)
             #progeny[j].cfd()
             Airfoil.append(progeny[j])
+
+        #for t in range(len(Airfoil)):
+        #print('after reproduction')
+        #print(len(Airfoil))
+        #print()
